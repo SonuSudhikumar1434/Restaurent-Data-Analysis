f6bc6d212a84f3df9a3a6448c4f3e1b2e26e4a3c
sonu <sonusudhikumarjobs@gmail.com>
2026-03-17 13:21:09 +0530

    Initial commit - Restaurant Data Analysis project

---
+++ sql/bronze/order_details.sql
@@ -0,0 +1,7 @@
+CREATE VIEW  IF NOT EXISTS bronze.order_details AS
+SELECT *
+FROM read_csv_auto(
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vR5A_FyTqQ9wUvXsFrE9UuKLg7mzqN7PdOr-IY8Th1vOwwkN9iV4HNxqYHtLVRR8ko_PZ_YDGmNoiuN/pub?gid=731024366&single=true&output=csv'
+);
+select * from Bronze.order_details
+;
